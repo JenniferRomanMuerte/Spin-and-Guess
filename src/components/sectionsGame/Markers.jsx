757136b4f49e644792dc23d30b020870681f2e6e
Jennifer <jenniferromanmuerte@gmail.com>
2025-12-24 12:11:36 +0100

    Added dynamic player name to the scoreboards

--- src/components/sectionsGame/Markers.jsx
+++ src/components/sectionsGame/Markers.jsx
@@ -1,10 +1,17 @@
+import { useLocation } from "react-router-dom";
 import "../../styles/layout/sectionGame/MarkersGame.scss";
 
 const Markers = () => {
+
+  const location = useLocation();
+
+  // Recuperamos el valor el nombre del jugador que está guardado en el estado
+  const namePlayer = location.state?.namePlayer;
+
   return (
     <article className="markers">
       <section className="markers__section">
-        <p className="markers__section--players">Jennifer</p>
+        <p className="markers__section--players">{namePlayer}</p>
         <div className="markers__section--score">000</div>
       </section>
       <section className="markers__section">
