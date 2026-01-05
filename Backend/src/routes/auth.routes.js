d793676f1e241534d5463a0f97ab701a50dedf1d
Jennifer <jenniferromanmuerte@gmail.com>
2026-01-05 19:45:22 +0100

    Added endpoint and function for valid token verification for session in Backend

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const router = express.Router();
 const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+const router = express.Router();
 
 router.post("/register", authController.register);
+
 router.post("/login", authController.login);
+
+router.get("/me", authMiddleware, (req, res) => {
+  res.json({
+    user: req.user,
+  });
+});
+
 
 module.exports = router;
--- Backend/src/utils/jwt.js
+++ Backend/src/utils/jwt.js
@@ -3,7 +3,9 @@ const jwt = require("jsonwebtoken");
 const jwtSecret = "secret_key";
 
 const generateToken = (payload) => {
-  return jwt.sign(payload, jwtSecret);
+  return jwt.sign(payload, jwtSecret, {
+    expiresIn: "24h",
+  });
 };
 
 const verifyToken = (token) => {
