1ce5a68e13e7d09dd651181f5d0d1a68f57f8a09
Jennifer <jenniferromanmuerte@gmail.com>
2026-01-16 22:52:45 +0100

    Added database in Neon and delete folder docs

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,12 +1,19 @@
 // Importar la biblioteca de Postgree
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.PG_HOST || "localhost",
-  port: process.env.PG_PORT || 5432,
-  user: process.env.PG_USER || "postgres",
-  password: process.env.PG_PASSWORD || "my_password",
-  database: process.env.PG_DATABASE || "roulette_game",
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      host: process.env.PG_HOST || "localhost",
+      port: process.env.PG_PORT || 5432,
+      user: process.env.PG_USER || "postgres",
+      password: process.env.PG_PASSWORD || "my_password",
+      database: process.env.PG_DATABASE || "roulette_game",
+    });
 
 module.exports = pool;
